f3abf476206bb9d82542726a1f6bab92273c0a72
Lee, Jiwoo <zizii23027@gmail.com>
2025-05-21 09:13:14 +0900

    five

--- src/g201623027/Main.java
+++ src/g201623027/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) { 
 		System.out.println("이지우");
-		World.run(); 
+		World.run();
+		World.run2();
 	}
 }
--- src/g201623027/World.java
+++ src/g201623027/World.java
@@ -3,4 +3,8 @@ public class World {
     public static void run() { 
         System.out.println("201623027"); 
     } 
+    
+    public static void run2() { 
+    	System.out.println("hello world"); 
+    }
 }
